65e324758e18f56eab38dd600e92c0c7f0083e70
MahdiKrimi <mahdi-smile@hotmail.com>
2021-06-10 17:54:52 -0700

    first move

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Address() {
+    return (
+        <div>
+            <h1><center>Address: Tunis, mnihla</center></h1>
+        </div>
+    )
+}
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PP from './ProfilePhoto.jpg'
+import './ProfilePhoto.css'
+
+export default function ProfilePhoto() {
+    return (
+        <div>
+           <img src={PP} className='drep'/>
+        </div>
+    )
+}
